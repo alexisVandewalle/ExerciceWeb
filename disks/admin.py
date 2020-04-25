83ed125d7db055c12d10a45cd36feefe997b480f
Alexis Vandewalle <a.vandewalle@isae.fr>
2020-04-25 12:19:15 +0200

    ajout de l'administration

--- disks/admin.py
+++ disks/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from disks.models import *
 
 # Register your models here.
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('Title','artist')
+    search_fields = ('Title',)
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('Name',)
+    search_fields = ('Name',)
+
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ('Name','Composer','UnitPrice')
+    search_fields = ('Name','Composer')
+
+admin.site.register(Album,AlbumAdmin)
+admin.site.register(Artist,ArtistAdmin)
+admin.site.register(Track,TrackAdmin)
--- disks/models.py
+++ disks/models.py
@@ -5,9 +5,20 @@ class Album(models.Model):
     Title = models.CharField(max_length=160)
     artist =models.ForeignKey('Artist', on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = 'album'
+
+    def __str__(self):
+        return self.Title
+
 class Artist(models.Model):
     Name = models.CharField(max_length=120)
 
+    class Meta:
+        verbose_name = "artist"
+
+    def __str__(self):
+        return self.Name
 class Track(models.Model):
     Name = models.CharField(max_length=200)
     Composer = models.CharField(max_length=220)
@@ -15,3 +26,9 @@ class Track(models.Model):
     Bytes = models.IntegerField()
     UnitPrice = models.DecimalField(max_digits=10,decimal_places=2)
     album = models.ForeignKey('Album', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name="track"
+
+    def __str__(self):
+        return self.Name
